90b29f202d419d8be35d1616f8dd23287b185aa2
Alex <75201898+Carlabiete@users.noreply.github.com>
2025-11-13 22:15:36 +0300

    Update InfoSection.jsx

--- src/components/InfoSection.jsx
+++ src/components/InfoSection.jsx
@@ -16,6 +16,8 @@ function InfoSection({
           <span style={{ opacity: 0.7 }}>Source : </span>
           <a
             href={planetData[currentTab].source}
+            target="_blank"
+            rel="noopener noreferrer"
             className="hyperlink opacity-70-hover"
           >
             Wikipedia
@@ -39,3 +41,4 @@ let text =
   "Third planet from the Sun and the only known planet to harbor life. About 29.2% of Earth's surface is land with remaining 70.8% is covered with water. Earth's distance from the Sun, physical properties and geological history have allowed life to evolve and thrive.";
 
 export default InfoSection;
+
